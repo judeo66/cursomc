14d1091dd5e5fea019a070d5c72c63c15d31b9d2
judeo66 <israel.0123@hotmail.com>
2018-11-18 20:09:56 -0200

    adicionado primeiro objeto categora, chama que é noix

--- cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 // O pacote resource é o pacote de controllers no rest
 @RestController
@@ -12,8 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST funfa cachorro";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
-
 }

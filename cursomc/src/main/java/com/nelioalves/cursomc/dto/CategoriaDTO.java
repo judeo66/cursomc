66f4c50fc21b1872900c9c2628ac1158eb884fb7
israel <israel@israel-A320MH-PRO>
2018-12-09 15:38:00 -0200

    adicionado validação nos objetos de entrada DTO

--- cursomc/src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.nelioalves.cursomc.domain.Categoria;
 
 
@@ -11,6 +15,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="Tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {

5c6cf3fac3c18a044e340131af96fdb0fd45bf88
israel <israel@israel-A320MH-PRO>
2018-12-09 14:07:55 -0200

    Adicionado primeiro dto

--- cursomc/src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -71,6 +71,11 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "informática");
 		Categoria cat2 = new Categoria(null, "escritório");
+		Categoria cat3 = new Categoria(null, "escritório");
+		Categoria cat4 = new Categoria(null, "escritório");
+		Categoria cat5 = new Categoria(null, "escritório");
+		Categoria cat6 = new Categoria(null, "escritório");
+		Categoria cat7 = new Categoria(null, "escritório");
 		
 		Produto p1 = new Produto(null, "computador", 2000.00);
 		Produto p2 = new Produto(null, "impressora", 800.00);
@@ -97,7 +102,7 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2));
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "123456879", TipoCliente.PESSOAFISICA);
